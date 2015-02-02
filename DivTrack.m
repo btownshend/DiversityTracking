07d424b242b9d69cd20ad2f21560411357c14cd9
Brent Townshend <bst@tc.com>
2015-02-02 15:00:51 -0800

    Updated S2.m

---
+++ DivTrack.m
@@ -0,0 +1,177 @@
+% DivTrack - track diversity of a series of rounds of TRP
+
+classdef DivTrack < matlab.mixin.Copyable
+  properties
+    volume; % Volume in ul
+    ngood;  % Number of good switch molecules in mixture
+    nragged;	% Number with incorrect ends (due to short oligos) - first of the two elements refers to 5' end of RNA (or corresponding end of DNA)
+    nbad;   % Number of other molecules in mixture (i.e. non-switches)
+    kgood;  % Avg. number of copies of each sequence in good molecules
+    initfracgood;
+    initngood;
+    tgtCleave;
+    prefix;
+    history;
+    cumcost;
+  end
+
+  methods(Static)
+    function m=moles(vol,conc)
+      m=vol*1e-6*conc*1e-9*6.022e23;
+    end
+  end
+  
+  methods
+    function obj=DivTrack(initvol, initconc, initfracgood,initfracragged,prefix,tgtCleave)
+      obj.volume=initvol;
+      obj.kgood=1;
+      obj.nragged=obj.moles(initvol,initconc)*initfracragged;
+      obj.ngood=obj.moles(initvol,initconc)*initfracgood*(1-sum(initfracragged));
+      obj.nbad=obj.moles(initvol,initconc)*(1-initfracgood)*(1-sum(initfracragged));
+      obj.initfracgood=initfracgood*(1-sum(initfracragged));
+      obj.initngood=obj.ngood;
+      obj.prefix=prefix;
+      obj.tgtCleave=sort(tgtCleave);
+      obj.history=[];
+      fprintf('Initial prefix=%s, Target cleavage=[%.2f, %.2f], Initial good=%.2g ragged=[%.2g %.2g] bad=%.2g\n',obj.prefix,obj.tgtCleave,obj.ngood, obj.nragged, obj.nbad);
+      obj.printdiv('Initial');
+      obj.cumcost=0;
+    end
+
+    function f=fracgood(obj)
+      f=obj.ngood/(obj.ngood+obj.nbad+sum(obj.nragged));
+    end
+    
+    function c=conc(obj)
+    % Concentration in nM
+      c=(obj.ngood+obj.nbad+sum(obj.nragged))/(obj.volume*1e-6)/6.022e23*1e9;
+    end
+        
+    function printdiv(obj,note)
+      fprintf('%-50.50s   %2s %4.0ful %4.0fnM Total=%7.2g Ragged=[%2.0f%%,%2.0f%%] Enrich=%7.2g kgood=%8.2f goodseqs=%5.3g cumcost=$%3.0f\n',note,obj.prefix, obj.volume,obj.conc(),obj.nbad+sum(obj.nragged)+obj.ngood,obj.nragged/(obj.nbad+obj.ngood+sum(obj.nragged))*100,obj.fracgood()/obj.initfracgood, obj.kgood, obj.ngood/obj.kgood, obj.cumcost);
+      obj.history=[obj.history,struct('ngood',obj.ngood,'bad',obj.nbad,'ragged',obj.nragged,'goodseqs',obj.ngood/obj.kgood,'note',note)];
+    end
+
+    function plothistory(obj)
+      subplot(211);
+      semilogy([obj.history.bad],'-o');
+      hold on;
+      semilogy(arrayfun(@(z) z.ragged(1),obj.history),'-o');
+      semilogy(arrayfun(@(z) z.ragged(2),obj.history),'-o');
+      set(gca,'XTick',1:length(obj.history));
+      set(gca,'XTickLabel',{});
+      legend('bad','left ragged','right ragged');
+      
+      subplot(212);
+      semilogy([obj.history.ngood],'-o');
+      hold on;
+      semilogy([obj.history.goodseqs],'-o');
+      legend('good','good sequences');
+      set(gca,'XTick',1:length(obj.history));
+      set(gca,'XTickLabel',{obj.history.note});
+      set(gca,'XTickLabelRotation',15);
+      c=axis;
+      c(3)=0.1;
+      axis(c);
+    end
+    
+    function resample(obj,note,gain)
+      obj.kgood=obj.kgood*gain/(1-exp(-obj.kgood*gain));
+      obj.ngood=obj.ngood*gain;
+      obj.nbad=obj.nbad*gain;
+      obj.nragged=obj.nragged*gain;
+      obj.printdiv(sprintf('%s(gain=%.3f)',note,gain));
+    end
+
+    function dilute(obj,tgtConc,note)
+      obj.volume=obj.volume*obj.conc()/tgtConc;
+      if nargin<3
+        note='Dilute';
+      end
+      obj.printdiv(note);
+    end
+    
+    function diluteToVolume(obj, volume, note)
+      if nargin<3
+        note='Dilute';
+      end
+      obj.volume=volume;
+      obj.printdiv(note);
+    end
+    
+    function randchoose(obj,note,goodgain,badgain,raggedgain)
+      if nargin<4
+        badgain=goodgain;
+        raggedgain=goodgain;
+      end
+      if badgain>1.0 || badgain<0.0 || goodgain>1.0 || goodgain<0.0
+        error('Bad gains: %f, %f\n', goodgain, badgain);
+      end
+      % Assume that there are exactly k copies of each sequence (TODO: does this reasonably approximate?)
+      pmissedgood=(1-goodgain)^obj.kgood;
+      obj.kgood=obj.kgood*goodgain/(1-pmissedgood);
+      obj.ngood=obj.ngood*goodgain;
+      obj.nbad=obj.nbad*badgain;
+      obj.nragged=obj.nragged*raggedgain;
+      if goodgain==badgain
+        obj.printdiv(sprintf('%s(gain=%.3f)',note,goodgain));
+      else
+        obj.printdiv(sprintf('%s(gain=%.3fG,%.3fB,%.3fR)',note,goodgain,badgain,raggedgain));
+      end
+    end
+
+% Select using given cleavage rate for BAD sequences
+    function Select(obj,keepCleave,cleaveRate)
+      badgain=cleaveRate;
+      if keepCleave
+        % Compute badgain such that observed cleavage rate is obtained
+        obj.prefix='-';
+        obj.randchoose(sprintf('Select(Clv,clv=%.2f)',cleaveRate),obj.tgtCleave(2),badgain,0);
+      else
+        obj.prefix='W';
+        obj.randchoose(sprintf('Select(Unclv,clv=%.2f)',cleaveRate),1-obj.tgtCleave(1),badgain,1.0);
+      end
+    end
+    
+    function T7(obj,rnaconc)
+    % Assume ones with ragged 5 end won't transcribe
+      obj.cumcost=obj.cumcost+0.079*obj.volume;   % Price/ul of rx:  T7: .050 NTP .016, SuperaseIn .013
+      gain=rnaconc*(obj.ngood+obj.nbad+sum(obj.nragged))/(obj.conc()*(obj.ngood+obj.nbad+obj.nragged(2)));
+      obj.nragged(1)=0;
+      obj.resample('T7',gain);
+    end
+
+    function RT(obj, efficiency,primerRaggedFrac)
+      obj.cumcost=obj.cumcost+.236*obj.volume;
+      newRagged=(obj.ngood+obj.nbad)*efficiency*primerRaggedFrac;
+      obj.randchoose('RT',efficiency*(1-primerRaggedFrac));
+      obj.nragged(2)=newRagged;
+    end
+    
+    function ligate(obj, prefix, efficiency)
+      obj.prefix=prefix;
+      obj.randchoose(sprintf('ligate(%s)',prefix),efficiency);
+      obj.cumcost=obj.cumcost+.0002*obj.volume;
+    end
+    
+    function gain=PCR(obj,finalvol,finalconc,primersRaggedFrac)
+      obj.volume=finalvol;
+      gain=obj.moles(finalvol,finalconc)/obj.moles(obj.volume,obj.conc());
+      goodseqs=obj.ngood/obj.kgood;
+      % No change to kgood since sampling is uniform
+      newragged=primersRaggedFrac*(gain-1)*(obj.nbad+obj.ngood);
+      obj.nbad=obj.nbad*(1+(gain-1)*(1-sum(primersRaggedFrac)));
+      obj.ngood=obj.ngood*(1+(gain-1)*(1-sum(primersRaggedFrac)));
+      obj.nragged=newragged+obj.nragged;
+      obj.kgood=obj.ngood/goodseqs;
+      obj.cumcost=obj.cumcost+obj.volume*263/250/50;  % Kapa is $263/250U, uses 1U/50ul
+      obj.printdiv(sprintf('PCR (gain=%.3f)',gain));
+    end
+
+    function measured(obj,note,volume,conc)
+      obj.volume=volume;
+      gain=obj.moles(volume,conc)/obj.moles(obj.volume,obj.conc());
+      obj.randchoose(note,gain);
+    end
+  end
+end
--- S2.m
+++ S2.m
@@ -1,48 +1,83 @@
 % Initial DNA library used in first transcription
 geleff=0.5;
 coleff=0.8;
-ligeff=0.25;
+ligeff=0.5;
+rteff=0.9;
 fullfrac=1;
 pcrgain=8;
+ce=0.99;
+lib=161;
+badCleavage=0.3;
+pagepurity=0.92;
+pageoligos=false;
 
-fprintf('\nR1a. Gel selection of cleaved from PAGE-purified input\n');
-div=DivTrack(1000,50,1e-10,'W',[0.2,0.8]);
-div.T7(1500);
-div.diluteToVolume(1012);
-div.Select(true,.3);
-div.randchoose('gel efficiency',geleff);
-div.diluteToVolume(100,'Zymo-Clean');
-div.diluteToVolume(div.volume/0.7,'RT');
-div.randchoose('RT efficiency',0.8);
-div.diluteToVolume(1.1*div.volume,'Ligation');
-div.randchoose('lig efficiency',ligeff);
-div.dilute(250/pcrgain,'PCR Input dilution');
-div.PCR(div.volume,div.conc*pcrgain);
-
-return 
-
-fprintf('\nR1b. Ligation selection of cleaved from PAGE-purified input\n');
-div=DivTrack(500,200,1e-10,'A',[0.2,0.8]);
-div.T7(1510);
-div.randchoose('lig efficiency',ligeff,ligeff);
-div.Select(true,.3);
-div.dilute(250/2^4);
-div.PCR(div.volume,250);
-
-fullfrac=0.15;
-fprintf('\nR1c. Gel selection of cleaved from non-purified input with %.2f full-length\n',fullfrac);
-div2=DivTrack(500,200,1e-10*fullfrac,'A',[0.2,0.8]);
-gain=16;  % Amount of gain of full-length
-div2.dilute(250/gain);
-adj=gain/(fullfrac*gain+(1-fullfrac));
-fullfrac=fullfrac*adj;
-div2.fracgood=div2.fracgood*adj;
-div2.randchoose('PCR of full-length',fullfrac+(1-fullfrac)/gain);
-div2.PCR(div2.volume,250);
-div2.dilute(1510);
-div2.T7(1510);
-div2.Select(true,.3*fullfrac);
-div2.randchoose('gel efficiency',geleff,geleff);
-div2.randchoose('lig efficiency',ligeff,ligeff);
-div2.dilute(250/2^4);
-div2.PCR(div2.volume,250);
+for i=1:5
+  fprintf('\n');
+  if i>=4
+    pageoligos=true;
+  end
+  % Compute fraction of results that will be ragged when using each oligo
+  if pageoligos
+    bt600=1-pagepurity;
+    bt30=1-pagepurity;
+    bt88=1-pagepurity;
+    bt575=1-pagepurity;
+    bt1076=1-pagepurity;
+    bt654=(1-pagepurity)/2;  % Most will probably still splint
+  else
+    bt600=1-ce^((28-5)-10);
+    bt30=1-ce^(45-10);
+    bt88=1-ce^(23-10);
+    bt575=1-ce^(29-10);
+    bt1076=1-ce^62;
+    bt654=1-ce^((43-16)-8);
+  end
+  fprintf('Fraction of product from each oligo that will be ragged on 5'' end:\n');
+  fprintf('BT600=%.2f, BT30=%.2f, BT88=%.2f, BT575=%.2f, BT1076=%.2f, BT654=%.2f\n', bt600,bt30,bt88,bt575,bt1076,bt654);
+  if i==1
+    ti=sprintf('R1a. Gel selection of cleaved from PAGE-purified inputs\n');
+    fprintf(ti);
+    vol=1000; % High enough to keep kgood>=1.5
+             % Start with at least 1.5 copies of good seqs
+    div=DivTrack(vol,90000/vol,1e-13*badCleavage,[1-pagepurity,1-pagepurity],'W',[0.2,0.8]);
+  elseif i==2
+    ti=sprintf('R1b. Gel selection of cleaved from non-PAGE-purified inputs\n');
+    fprintf(ti);
+    div=DivTrack(7000,400,1e-13*badCleavage,[bt88,1-ce^lib],'W',[0.2,0.8]);
+  elseif i==3
+    ti=sprintf('R1c. Gel selection of cleaved from PCR products of non-PAGE-purified inputs\n');
+    fprintf(ti);
+    div=DivTrack(4000,105,1e-13*badCleavage,[0.0,1-ce^lib],'W',[0.2,0.8]);
+    div.PCR(div.volume,div.conc*8,[bt88,bt575]);
+    div.volume=div.volume/4; div.randchoose('Part of PCR',0.25);
+    div.diluteToVolume(2000,'T7 input dilution');
+  elseif i==4
+    ti=sprintf('R1d. Gel selection of cleaved from PCR products of non-PAGE-purified inputs using PAGE-oligos\n');
+    fprintf(ti);
+    div=DivTrack(2500,155,1e-13*badCleavage,[0.0,1-ce^lib],'W',[0.2,0.8]);
+    div.PCR(div.volume,div.conc*4,[bt88,bt575]);
+    div.volume=div.volume/2; div.randchoose('Part of PCR',0.5);
+    div.diluteToVolume(900,'T7 input dilution');
+  elseif i==5
+    ti=sprintf('R1e. Gel selection of cleaved from PCR products of PAGE-purified inputs using PAGE-oligos\n');
+    fprintf(ti);
+    div=DivTrack(1500,65,1e-13*badCleavage,[0.0,1-pagepurity],'W',[0.2,0.8]);
+    div.PCR(div.volume,div.conc*4,[bt88,bt575]);
+    div.volume=div.volume/2; div.randchoose('Part of PCR',0.5);
+    div.diluteToVolume(370,'T7 input dilution');
+  end
+  div.T7(1500);   % Assumed yield from T7 is 1.5uM, independent of template concentration
+  
+  div.Select(true,badCleavage);
+  div.randchoose('gel efficiency',geleff);
+  div.diluteToVolume(100,'Zymo-Clean concentrate');
+  div.diluteToVolume(div.volume/0.7,'RT input dilution');
+  div.RT(rteff,bt600);
+  div.diluteToVolume(1.1*div.volume,'Ligation input dilution');
+  div.ligate('BW',ligeff*(1-bt1076)*(1-bt654));
+  div.dilute(250/pcrgain,'PCR Input dilution');
+  div.PCR(div.volume,div.conc*pcrgain,[bt30,bt575]);
+  setfig(sprintf('R1-%d',i)); clf;
+  div.plothistory(); suptitle(sprintf('%s $%.0f/lib',ti,div.cumcost));
+  %fprintf('Total cost for 10 libraries:  $%.0f\n', div.cumcost*10);
+end
