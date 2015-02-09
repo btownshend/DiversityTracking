7f37ead505fd01d4bd3634bf12d2d744a16c31e3
Brent Townshend <bst@tc.com>
2015-02-08 21:19:11 -0800

    Split out divsetup.m

---
+++ bt1166p.m
@@ -0,0 +1,35 @@
+% Track 1/28/15 large scale test of N5N60 TRP
+% https://www.evernote.com/shard/s240/nl/4571194/6ee8e7ed-c546-4b93-bf82-87278a5219f5/
+divsetup
+
+goodFrac=1e-13*.171;    % Assume good molecules (i.e. aptamers) occur at the rate of 10^-13 of CLEAVABLE molecules
+template=1-ceultramer^161;
+templatep=(1-pagepurity)*template;
+
+fprintf('\nPilot Run 1/28/15:\n');
+div=DivTrack(1000,50,goodFrac,[bt88,templatep],'W',[0.2,0.8]);
+div.T7(div.conc()*39.15); 
+div.measured('Post-T7',49,39150);
+div.Select(true,0.171);
+div.measured('Post-PAGE',32,4231);
+div.RT(1.0,bt600);
+div.measured('Post-Ligation',704,30);
+
+% Now run planned setup
+fprintf('\nPlanned:\n');
+div=DivTrack(1000,150,goodFrac,[bt88,templatep],'W',[0.2,0.8]);
+div.T7(1958);
+div.volume=50;
+div.Select(true,0.171);
+div.volume=50;
+div.randchoose('Post-PAGE',.454);
+div.RT(1.0,bt600);
+div.volume=div.volume*12;
+div.randchoose('Post-Ligation',.156*2);
+% PCR using page-purified BT575
+pcrgain=8;
+div.dilute(250/pcrgain,'Pre-PCR dilution');
+div.PCR(div.volume,div.conc*pcrgain,[bt28,bt575p]);
+
+div.randchoose('Use part for next round',2/pcrgain);
+div.T7(1958);
