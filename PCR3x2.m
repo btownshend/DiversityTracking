42b748350d9e5ffce9430ffaa0cba3cc3308bbd1
Brent Townshend <bst@tc.com>
2015-02-08 23:55:44 -0800

    Setup for PCR of dsDNA

--- PCR3x2.m
+++ PCR3x2.m
@@ -4,8 +4,8 @@ fracGood=.12;
 badCleavage=0.171;
 
 div=DivTrack(480,125/fracGood,1e-13*badCleavage,[0,1-fracGood],'W',[0.2,0.8]);
-div.PCR(1,[bt88,bt575]);
+div.PCR(2,[bt88,bt575]);
 div.resample('keep 1/2',0.5);
-div.PCR(1,[bt88,bt575]);
+div.PCR(1,[bt88,bt575],true);
 div.resample('keep 1/2',0.5);
-div.PCR(1,[bt88,bt575]);
+div.PCR(1,[bt88,bt575],true);
