473bd19e0509a63df0722c55347b70073e93107c
Brent Townshend <bst@tc.com>
2017-03-02 10:03:12 -0800

    Removed ragged processing

--- TRPTrack.m
+++ TRPTrack.m
@@ -4,7 +4,6 @@ classdef TRPTrack < matlab.mixin.Copyable
   properties
     volume; % Volume in ul
     ngood;  % Number of good switch molecules in mixture
-    nragged;	% Number with incorrect ends (due to short oligos) - first of the three elements refers to 5' end of RNA (or corresponding end of DNA), (2) is 3' end, (3) is both ends ragged
     nbad;   % Number of other molecules in mixture (i.e. non-switches)
     initfracgood;
     initngood;
@@ -26,22 +25,16 @@ classdef TRPTrack < matlab.mixin.Copyable
   
   
   methods
-    function obj=TRPTrack(initvol, initconc, initfracgood,initfracragged,prefix,tgtCleave,poolCleavage)
+    function obj=TRPTrack(initvol, initconc, initfracgood,prefix,tgtCleave,poolCleavage)
     % initialize pool with given volume (in ul) and concentration (in nM)
     % initfracgood is the fraction of the total pool that is "good"
-    % initfracragged is a two-element vector providing the fraction of the pool that is ragged on the left and right ends
     % prefix is the prefix of the library;  'W','AW','BW','-'(cleaved);  just for keeping track
     % tgtCleave is the cleavage with and without target for the "good" sequences      
     % poolFitness is an empirical distribution of the cleavage of the pool, each value is equally probable
       obj.volume=initvol;
-      if length(initfracragged)==2
-        initfracragged(3)=prod(initfracragged); % Both ends rough (assume independence)
-        initfracragged(1:2)=initfracragged(1:2)-initfracragged(3);
-      end
-      obj.nragged=obj.moles(initvol,initconc)*initfracragged;
-      obj.ngood=obj.moles(initvol,initconc)*initfracgood*(1-sum(initfracragged));
-      obj.nbad=obj.moles(initvol,initconc)*(1-initfracgood)*(1-sum(initfracragged));
-      obj.initfracgood=initfracgood*(1-sum(initfracragged));
+      obj.ngood=obj.moles(initvol,initconc)*initfracgood;
+      obj.nbad=obj.moles(initvol,initconc)*(1-initfracgood)
+      obj.initfracgood=initfracgood;
       obj.initngood=obj.ngood;
       obj.randtargets=1000;
       obj.goodseqs=1:obj.randtargets;
@@ -50,7 +43,7 @@ classdef TRPTrack < matlab.mixin.Copyable
       obj.history=[];
       obj.setPoolCleavage(poolCleavage);
       obj.poolFrac=ones(size(poolCleavage))/length(poolCleavage);
-      fprintf('Initial prefix=%s, Target cleavage=[%.2f, %.2f], Initial good=%.3g ragged=[%.2g %.2g %.2g] bad=%.2g\n',obj.prefix,obj.tgtCleave,obj.ngood, obj.nragged, obj.nbad);
+      fprintf('Initial prefix=%s, Target cleavage=[%.2f, %.2f], Initial good=%.3g bad=%.2g\n',obj.prefix,obj.tgtCleave,obj.ngood, obj.nbad);
       obj.printdiv('Initial');
       obj.cumcost=0;
     end
@@ -72,7 +65,7 @@ classdef TRPTrack < matlab.mixin.Copyable
     end
     
     function t=total(obj)
-      t=obj.ngood+obj.nbad+sum(obj.nragged);
+      t=obj.ngood+obj.nbad;
     end
 
     function f=fracgood(obj)
@@ -85,19 +78,17 @@ classdef TRPTrack < matlab.mixin.Copyable
     end
         
     function printdiv(obj,note)
-      fprintf('%-50.50s   %2s %5.0ful %6.1fnM Total=%7.2g Ragged=[%2.0f%%,%2.0f%%,%2.0f%%] Enrich=%7.2g kgood=%8.2f goodseqs=%5.3g cumcost=$%3.0f\n',note,obj.prefix, obj.volume,obj.conc(),obj.total(),obj.nragged/obj.total()*100,obj.fracgood()/obj.initfracgood, obj.kgood(), obj.divtarget(), obj.cumcost);
-      obj.history=[obj.history,struct('ngood',obj.ngood,'bad',obj.nbad,'ragged',obj.nragged,'divtarget',obj.divtarget,'note',note)];
+      fprintf('%-50.50s   %2s %5.0ful %6.1fnM Total=%7.2g Enrich=%7.2g kgood=%8.2f goodseqs=%5.3g cumcost=$%3.0f\n',note,obj.prefix, obj.volume,obj.conc(),obj.total(),obj.fracgood()/obj.initfracgood, obj.kgood(), obj.divtarget(), obj.cumcost);
+      obj.history=[obj.history,struct('ngood',obj.ngood,'bad',obj.nbad,'divtarget',obj.divtarget,'note',note)];
     end
 
     function plothistory(obj)
       subplot(211);
       h=semilogy([obj.history.bad],'-o');
       hold on;
-      h(2)=semilogy(arrayfun(@(z) z.ragged(1),obj.history),'-o');
-      h(3)=semilogy(arrayfun(@(z) z.ragged(2),obj.history),'-o');
       set(gca,'XTick',1:length(obj.history));
       set(gca,'XTickLabel',{});
-      legend(h,{'bad','left ragged','right ragged'});
+      legend(h,{'bad'});
       
       subplot(212);
       h=semilogy([obj.history.ngood],'-o');
@@ -131,7 +122,6 @@ classdef TRPTrack < matlab.mixin.Copyable
     % Resample (with replacement) pool with given gain
       obj.ngood=obj.ngood*gain;
       obj.nbad=obj.nbad*gain;
-      obj.nragged=obj.nragged*gain;
       obj.goodseqs=randsample(obj.goodseqs,round(gain*length(obj.goodseqs)),true);
       obj.printdiv(sprintf('%s(gain=%.3f)',note,gain));
     end
@@ -152,25 +142,23 @@ classdef TRPTrack < matlab.mixin.Copyable
       obj.printdiv(note);
     end
     
-    function randchoose(obj,note,goodgain,badgain,raggedgain)
+    function randchoose(obj,note,goodgain,badgain)
     % Choose from the pool without replacement
     % A different gain can be applied to each molecule class, each of which should be between 0 and 1
       if nargin<4
         badgain=goodgain;
-        raggedgain=goodgain;
       end
-      if badgain>1.0 || badgain<0.0 || goodgain>1.0 || goodgain<0.0 || any(raggedgain<0) || any(raggedgain>1)
-        error('Bad gains: %f, %f, %f, %f\n', goodgain, badgain, raggedgain);
+      if badgain>1.0 || badgain<0.0 || goodgain>1.0 || goodgain<0.0
+        error('Bad gains: %f, %f, %f\n', goodgain, badgain);
       end
       % Assume that there are exactly k copies of each sequence (TODO: does this reasonably approximate?)
       obj.ngood=obj.ngood*goodgain;
       obj.nbad=obj.nbad*badgain;
-      obj.nragged=obj.nragged*raggedgain;
       obj.goodseqs=randsample(obj.goodseqs,round(goodgain*length(obj.goodseqs)),false);
       if goodgain==badgain
         obj.printdiv(sprintf('%s(gain=%.3f)',note,goodgain));
       else
-        obj.printdiv(sprintf('%s(gain=%.3fG,%.3fB,%.3fR)',note,goodgain,badgain,raggedgain));
+        obj.printdiv(sprintf('%s(gain=%.3fG,%.3fB)',note,goodgain,badgain));
       end
     end
 
@@ -190,22 +178,20 @@ classdef TRPTrack < matlab.mixin.Copyable
     function Select(obj,keepCleave,gamma)
     % Choose molecules from the pool that either cleave or not (depending on keepCleave)
     % The cleavage rate of the bad molecules is based on the empirical distribution poolCleavage and poolFrac.  
-    % OLD: Ragged molecules never cleave and good molecules cleave with tgtCleave
-    % With loopback: Ragged carries through (will affect subsequent extensions)
     % Cleavage values are mapped to values that result in the observed retentionFraction
       if keepCleave
         retain=1-(1-obj.poolCleavage).^gamma;
         tgtRetain=1-(1-obj.tgtCleave(2))^gamma;
         meanRetain=sum(retain.*obj.poolFrac);
         obj.prefix='-';
-        obj.randchoose(sprintf('Select(Clv,%.1f)',gamma),tgtRetain,meanRetain,meanRetain);
+        obj.randchoose(sprintf('Select(Clv,%.1f)',gamma),tgtRetain,meanRetain);
       else
         obj.prefix='W';
         fprintf('Gamma=%.1f\n',gamma);
         retain=(1-obj.poolCleavage).^gamma;
         tgtRetain=(1-obj.tgtCleave(1))^gamma;
         meanRetain=sum(retain.*obj.poolFrac);
-        obj.randchoose(sprintf('Select(Unclv,%.1f)',gamma),tgtRetain,meanRetain,meanRetain);
+        obj.randchoose(sprintf('Select(Unclv,%.1f)',gamma),tgtRetain,meanRetain);
       end
       obj.poolFrac=obj.poolFrac.*retain;
       obj.poolFrac=obj.poolFrac/sum(obj.poolFrac);
@@ -213,11 +199,9 @@ classdef TRPTrack < matlab.mixin.Copyable
     
     function T7(obj,rnaconc)
     % Transcribe the pool ending with the given RNA concentration
-    % Assume molecules with ragged left end won't transcribe, but ragged right end ones are inherited
     % After this method, the pool will be referring to the RNA produced only, not including the template
       obj.cumcost=obj.cumcost+0.079*obj.volume;   % Price/ul of rx:  T7: .050 NTP .016, SuperaseIn .013
-      gain=rnaconc*obj.total()/(obj.conc()*(obj.ngood+obj.nbad+obj.nragged(2)));
-      obj.nragged([1,3])=0;
+      gain=rnaconc*obj.total()/(obj.conc()*(obj.ngood+obj.nbad));
       obj.resample('T7',gain);
     end
 
@@ -228,20 +212,10 @@ classdef TRPTrack < matlab.mixin.Copyable
       obj.cumcost=obj.cumcost+obj.volume*beadConc*cost;
     end
     
-    function RT(obj, efficiency,primerRaggedFrac)
+    function RT(obj, efficiency)
     % Reverse transcription
-    % bad, good, left-ragged all get same gain of efficiency*(1-primerRaggedFrac)
-    % right-ragged not amplified
-    % New right/both ragged based on primerRaggedFrac
+    % bad, good, all get same gain of efficiency
       obj.cumcost=obj.cumcost+.236*obj.volume;
-      % First assume that anything right-ragged won't be RT-ed
-      obj.nragged(2:3)=0;
-      % Now move some things to right-ragged
-      obj.nragged(2)=(obj.nbad+obj.ngood)*primerRaggedFrac;
-      obj.nbad=obj.nbad*(1-primerRaggedFrac);
-      obj.ngood=obj.ngood*(1-primerRaggedFrac);
-      obj.nragged(3)=obj.nragged(1)*primerRaggedFrac;
-      obj.nragged(1)=obj.nragged(1)*(1-primerRaggedFrac);
       % Apply the gain
       obj.randchoose('RT',efficiency);
     end
@@ -252,10 +226,9 @@ classdef TRPTrack < matlab.mixin.Copyable
       obj.cumcost=obj.cumcost+.0002*obj.volume;
     end
     
-    function gain=PCR(obj,ncycles,primersRaggedFrac,isds)
+    function gain=PCR(obj,ncycles,isds)
     % PCR amplify the pool to the given final volume and concentration
-    % Assumes perfect amplication and uniform copying of all non-ragged input molecules
-    % The resulting pool will inherit the ragged fraction of the primers + the original unamplified ragged ones
+    % Assumes perfect amplication and uniform copying of all input molecules
     % isds can be set to true to indicate the input is double-stranded; otherwise, if omitted, assumes input is single-stranded cDNA
       if nargin<4
         isds=false;
@@ -263,37 +236,31 @@ classdef TRPTrack < matlab.mixin.Copyable
       
       % Initial S1 to forward strand (same direction as RNA), S2 as reverse strand
       if isds
-        S1=struct('nbad',obj.nbad,'ngood',obj.ngood,'nragged',obj.nragged,'goodseqs',obj.goodseqs);
+        S1=struct('nbad',obj.nbad,'ngood',obj.ngood,'goodseqs',obj.goodseqs);
       else
-        if obj.nragged(1)>0 || obj.nragged(3)>0
-          fprintf('Warning: PCR of single-stranded cDNA, but left end has some ragged 5''-ends??\n');
-        end
-        S1=struct('nbad',0,'ngood',0,'nragged',[0,0,0],'goodseqs',[1]);  % Keep 1 sequence to avoid errors later
+        S1=struct('nbad',0,'ngood',0,'goodseqs',[1]);  % Keep 1 sequence to avoid errors later
       end
-      S2=struct('nbad',obj.nbad,'ngood',obj.ngood,'nragged',obj.nragged([2,1,3]),'goodseqs',obj.goodseqs);
+      S2=struct('nbad',obj.nbad,'ngood',obj.ngood,'goodseqs',obj.goodseqs);
 
       for i=1:ncycles
         % Update
-        %fprintf('S1=%.2g %.2g [%.2g %.2g] S2=%.2g %.2g [%.2g %.2g]\n', S1.nbad, S1.ngood, S1.nragged, S2.nbad, S2.ngood, S2.nragged);
-        S1new=struct('nbad',S1.nbad+S2.nbad*(1-primersRaggedFrac(1)),...
-                     'ngood',S1.ngood+S2.ngood*(1-primersRaggedFrac(1)),...
-                     'nragged',S1.nragged+[primersRaggedFrac(1)*(S2.ngood+S2.nbad),S2.nragged(1)*(1-primersRaggedFrac(1)),S2.nragged(1)*primersRaggedFrac(1)],...
-                     'goodseqs',[S1.goodseqs,randsample(S2.goodseqs,round(length(S2.goodseqs)*(1-primersRaggedFrac(1))))]);
-        S2new=struct('nbad',S2.nbad+S1.nbad*(1-primersRaggedFrac(2)),...
-                     'ngood',S2.ngood+S1.ngood*(1-primersRaggedFrac(2)),...
-                     'nragged',S2.nragged+[primersRaggedFrac(2)*(S1.ngood+S1.nbad),S1.nragged(1)*(1-primersRaggedFrac(2)),S1.nragged(1)*primersRaggedFrac(2)],...
-                     'goodseqs',[S2.goodseqs,randsample(S1.goodseqs,round(length(S1.goodseqs)*(1-primersRaggedFrac(2))))]);
+        %fprintf('S1=%.2g %.2g S2=%.2g %.2g \n', S1.nbad, S1.ngood, S2.nbad, S2.ngood);
+        S1new=struct('nbad',S1.nbad+S2.nbad,...
+                     'ngood',S1.ngood+S2.ngood,...
+                     'goodseqs',[S1.goodseqs,randsample(S2.goodseqs,round(length(S2.goodseqs)))]);
+        S2new=struct('nbad',S2.nbad+S1.nbad,...
+                     'ngood',S2.ngood+S1.ngood,...
+                     'goodseqs',[S2.goodseqs,randsample(S1.goodseqs,round(length(S1.goodseqs)))]);
         S1=S1new;
         S2=S2new;
       end
       obj.goodseqs=S2.goodseqs;   % Reverse-complement strand is template for future transcription
-      fprintf('S1=%.2g %.2g [%.2g %.2g %.2g] S2=%.2g %.2g [%.2g %.2g %.2g]\n', S1.nbad, S1.ngood, S1.nragged, S2.nbad, S2.ngood, S2.nragged);
+      fprintf('S1=%.2g %.2g  S2=%.2g %.2g\n', S1.nbad, S1.ngood, S2.nbad, S2.ngood);
       % No change to number of good sequences since sampling is uniform
       oldtotal=obj.total;
       % During last cycle, assume everything got extended so all parameters are aveages
       obj.ngood=mean([S1.ngood,S2.ngood]);
       obj.nbad=mean([S1.nbad,S2.nbad]);
-      obj.nragged=(S1.nragged+S2.nragged)/2;
       gain=obj.total/oldtotal;
       obj.cumcost=obj.cumcost+obj.volume*263/250/50;  % Kapa is $263/250U, uses 1U/50ul
       obj.printdiv(sprintf('PCR (gain=%.3f)',gain));
